7d36713bdb77eec049554a7f0bb2657d0033ae9d
Zachary Spector <zjspector@gmail.com>
2014-04-29 10:43:49 -0500

    Game resets after win or tie

--- main.js
+++ main.js
@@ -20,6 +20,15 @@ var setNextTurn = function () {
   $('#turn-label').text(currentPlayer);
 };
 
+var tie = function() {
+  for (i=0; i < spaces.length; i++) {
+    if (!spaces[i]) {
+      return false;
+    }
+  }
+    return true;
+}
+
 var checkForWinner = function () {
   // Because (NaN === NaN) is always false, we can safely assume
   // that if three spaces in a row are the same, all three spaces are
@@ -37,22 +46,45 @@ var checkForWinner = function () {
   {
     console.log('somebody won');
     // TODO: Trigger 'game-win' event with the winning player as the event data
-    $(document).trigger('game-win', 'Jon')
+    $(document).trigger('game-win', currentPlayer);
+    spaces = [
+      NaN, NaN, NaN,
+      NaN, NaN, NaN,
+      NaN, NaN, NaN,
+    ];
+    $('#board .space').removeClass(player1);
+    $('#board .space').removeClass(player2);
+    setNextTurn();
+  } else if (tie()) {
+    alert("It's a tie!")
+    spaces = [
+      NaN, NaN, NaN,
+      NaN, NaN, NaN,
+      NaN, NaN, NaN,
+    ];
+    $('#board .space').removeClass(player1);
+    $('#board .space').removeClass(player2);
+    setNextTurn();
   }
 };
 
 $(document).on('click', '#board .space', function (e) {
+  e.preventDefault
   var spaceNum = $(e.currentTarget).index();
   console.log('You clicked on space #' + spaceNum);
 
   // Marks the space with the current player's name
   // TODO: Don't mark it unless the space is blank
-  spaces[spaceNum] = currentPlayer;
-  // Adds a class to elem so css can take care of the visuals
-  $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
+  if (spaces[spaceNum]) {
+    alert("That space has been chosen already!");
+  } else {
+    spaces[spaceNum] = currentPlayer;
+    // Adds a class to elem so css can take care of the visuals
+    $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
 
-  checkForWinner();
-  setNextTurn();
+    checkForWinner();
+    setNextTurn();
+  }
 });
 
 $(document).on('game-win', function (e, winner) {
